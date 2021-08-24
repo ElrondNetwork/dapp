157ed68b2c95e85800603a03a0231c8fa49463d4
Radu Mojic <radu.mojic@elrond.com>
2021-08-24 14:39:41 +0300

    use signTransactions from provider

--- src/components/SignTransactions/SignWithWalletConnectModal/index.tsx
+++ src/components/SignTransactions/SignWithWalletConnectModal/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { Modal } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
-import { Transaction, Address, Nonce } from "@elrondnetwork/erdjs";
-import { Signature } from "@elrondnetwork/erdjs/out/signature";
+import { Transaction } from "@elrondnetwork/erdjs";
 import { faHourglass, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "context";
 import { HandleCloseType } from "../helpers";
@@ -31,7 +30,7 @@ const SignWithWalletConnectModal = ({
   const history = useHistory();
   const context = useContext();
 
-  const { dapp, address } = context;
+  const { dapp } = context;
   const provider: any = dapp.provider;
 
   const [signedTransactions, setSignedTransactions] = React.useState<
@@ -51,90 +50,24 @@ const SignWithWalletConnectModal = ({
 
   React.useEffect(() => {
     if (transactions && transactions.length) {
-      if (transactions.length === 1) {
-        const transaction = transactions[0];
-        dapp.proxy
-          .getAccount(new Address(address))
-          .then((account) => {
-            transaction.setNonce(new Nonce(account.nonce.valueOf()));
-            provider
-              .init()
-              .then((initialised: boolean) => {
-                if (!initialised) {
-                  return;
-                }
-                provider
-                  .signTransaction(transaction)
-                  .then((tx: Transaction) => {
-                    const newSignedTx = { [0]: tx };
-                    setSignedTransactions((existing) =>
-                      existing ? { ...existing, ...newSignedTx } : newSignedTx
-                    );
-                  });
-              })
-              .catch((e: any) => {
-                console.error("Failed initializing provider ", e);
-              });
-          })
-          .catch((e) => {
-            setError(e.message);
-          });
-      } else {
-        const transactionsDetails = transactions.map((transaction) => {
-          return {
-            nonce: transaction.getNonce().valueOf(),
-            from: address.toString(),
-            to: transaction.getReceiver().toString(),
-            amount: transaction.getValue().toString(),
-            gasPrice: transaction.getGasPrice().valueOf().toString(),
-            gasLimit: transaction.getGasLimit().valueOf().toString(),
-            data: Buffer.from(
-              transaction.getData().toString().trim()
-            ).toString(),
-            chainId: transaction.getChainID().valueOf(),
-            version: transaction.getVersion().valueOf(),
-          };
+      provider
+        .init()
+        .then((initialised: boolean) => {
+          if (!initialised) {
+            return;
+          }
+          provider
+            .signTransactions(transactions)
+            .then((transactions: Transaction[]) => {
+              setSignedTransactions(transactions);
+            })
+            .catch((e: any) => {
+              setError(e.message);
+            });
+        })
+        .catch((e: any) => {
+          setError(e.message);
         });
-
-        provider
-          .init()
-          .then((initialised: boolean) => {
-            if (!initialised) {
-              return;
-            }
-            provider
-              .sendCustomMessage({
-                method: "erd_batch_sign",
-                params: transactionsDetails,
-              })
-              .then((signatures: any) => {
-                signatures.forEach((signature: any, key: number) => {
-                  const transaction = transactions[key];
-                  transaction.applySignature(
-                    new Signature(signature.signature),
-                    new Address(address)
-                  );
-                  const newSignedTx = {
-                    [key]: transaction,
-                  };
-                  setSignedTransactions((existing) =>
-                    existing
-                      ? {
-                          ...existing,
-                          ...newSignedTx,
-                        }
-                      : newSignedTx
-                  );
-                });
-              })
-              .catch(() => {
-                handleClose();
-              });
-          })
-          .catch((e: any) => {
-            console.error("Failed initializing provider ", e);
-          });
-      }
     }
   }, [transactions]);
 
