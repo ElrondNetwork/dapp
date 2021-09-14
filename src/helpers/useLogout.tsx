625157b258a1fdca3bf36903b9d26cd39389386a
Tudor Morar <tudor.morar@elrond.com>
2021-09-14 17:41:47 +0300

    Only call dapp provider logout if provider is set

--- src/helpers/useLogout.tsx
+++ src/helpers/useLogout.tsx
@@ -1,21 +1,26 @@
 import { useContext, useDispatch } from "context";
+import getProviderType from "./getProviderType";
 import storage from "./storage";
 
 export default function useLogout() {
   const { dapp } = useContext();
   const dispatch = useDispatch();
+  const providerType = getProviderType(dapp.provider);
+
   return ({ callbackUrl }: { callbackUrl: string }) => {
     storage.session.clear();
     storage.local.removeItem("nonce");
     storage.local.removeItem("address");
     storage.local.removeItem("loginMethod");
-    dapp.provider
-      .logout({ callbackUrl })
-      .then(() => {
-        dispatch({ type: "logout" });
-      })
-      .catch((e) => {
-        console.error("Unable to perform logout", e);
-      });
+    if (Boolean(providerType)) {
+      dapp.provider
+        .logout({ callbackUrl })
+        .then(() => {
+          dispatch({ type: "logout" });
+        })
+        .catch((e) => {
+          console.error("Unable to perform logout", e);
+        });
+    }
   };
 }
